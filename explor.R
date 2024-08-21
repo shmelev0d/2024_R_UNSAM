11efc8df734748e1e1b33a2bd05b4d41e9e9dba9
Andrei <anevskij@gmail.com>
2024-08-20 21:20:54 -0300

    Update explor.R

--- explor.R
+++ explor.R
@@ -16,27 +16,37 @@ df_clae2_promedio
 
 
 # join data
-df_clae2_promedio <- df_clae2_promedio %>%
-  left_join(df_clae2, by ="clae2") 
-
-df_clae2_promedio <- df_clae2_promedio %>%
-  left_join(df_depart, by = c("codigo_departamento_indec", "id_provincia_indec"))
-
-df_clae2_promedio <- df_clae2_promedio %>% 
+df_clae2_promedio_joint <- df_clae2_promedio %>%
+  left_join(df_clae2, by ="clae2") %>%
+  left_join(df_depart, by = c("codigo_departamento_indec", "id_provincia_indec")) %>%
+  rename(cod_depto = codigo_departamento_indec, 
+         id_provincia = id_provincia_indec, 
+         nom_depto = nombre_departamento_indec, 
+         nom_provincia = nombre_provincia_indec) %>%
   mutate(year = year(fecha))
 
+
+
 # EDA
-df_clae2_promedio %>% 
+df_clae2_promedio_joint %>% 
   select(year, 
          w_mean, 
          clae2_desc, 
          letra_desc, 
-         nombre_departamento_indec,
-         nombre_provincia_indec) %>%
-  filter(nombre_provincia_indec == "Cordoba", clae2_desc == "Elaboración de bebidas")
+         nom_depto,
+         nom_provincia) %>%
+  filter(nom_provincia == "Cordoba", clae2_desc == "Elaboración de bebidas")
 
+min(df_clae2_promedio_joint$w_mean)
+head(sort(unique(df_clae2_promedio_joint$w_mean)), 10)
+max(df_clae2_promedio_joint$w_mean)
+mean(df_clae2_promedio_joint$w_mean)
+median(df_clae2_promedio_joint$w_mean)
 
-promedio_sectores <- df_clae2_promedio %>%
+df_clae2_promedio_joint %>%
+  filter(w_mean == -99)   # casi una cuarta de los registros
+
+promedio_sectores <- df_clae2_promedio_joint %>%
   group_by(letra_desc) %>%
   summarize(promedio = mean(w_mean))
 
