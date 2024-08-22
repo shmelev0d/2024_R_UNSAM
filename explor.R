3f4e564136d1d296b30b36b24b2086e0d386fbc5
Andrei <anevskij@gmail.com>
2024-08-22 19:54:59 -0300

    Update explor.R

--- explor.R
+++ explor.R
@@ -16,7 +16,8 @@ df_clae2_promedio
 
 
 
-# join data
+## join data
+
 df_clae2_promedio_joint <- df_clae2_promedio %>%
   left_join(df_clae2, by ="clae2") %>%
   left_join(df_depart, by = c("codigo_departamento_indec", "id_provincia_indec")) %>%
@@ -26,10 +27,10 @@ df_clae2_promedio_joint <- df_clae2_promedio %>%
          nom_provincia = nombre_provincia_indec) %>%
   mutate(year = year(fecha))
 
+df_clae2_promedio_joint
 
 
-
-# glimpse
+## glimpse
 
 min(df_clae2_promedio_joint$w_mean)
 head(sort(unique(df_clae2_promedio_joint$w_mean)), 10)
@@ -43,6 +44,8 @@ df_clae2_promedio_joint %>%
 df_clae2_promedio_joint %>%
   filter(w_mean == 154)   # outlier
 
+## variables para elegir los sectores
+
 
 # salario promedio para sectores (letra)
 
@@ -55,9 +58,19 @@ ggplot(promedio_sectores, aes(x = letra_desc, y = promedio)) +
   geom_col() + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 5))
 
+# salario promedio para los años (year)
+
+promedio_años <- df_clae2_promedio_joint %>%
+  group_by(year, letra_desc) %>%
+  summarize(promedio = mean(w_mean))
+
+
+ggplot(promedio_años, aes(x = letra_desc, y = promedio, fill = year)) +
+  geom_col() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 5))
+
 
 #la cantidad de NA (-99)
-
 
 num_NAs <- df_clae2_promedio_joint %>%
   group_by(letra_desc) %>%
@@ -74,6 +87,32 @@ ggplot(num_NAs, aes(x = letra_desc)) +
   theme(axis.text.x = element_blank(), axis.title = element_blank(), axis.ticks = element_blank())
 
 
+## reemplazamos las -99 con el promedio de los proximos valores usando interpolacíon
+
+# cambiamos los -99 a NA
+df_clae2_promedio_joint$w_mean[df_clae2_promedio_joint$w_mean == -99] <- NA
+
+# la tabla de indices de NAs
+na_indices <- which(is.na(df_clae2_promedio_joint$w_mean))
+
+# la tabla de proximos observaciones
+neighbors <- data.frame(
+  Index = na_indices,
+  Previous = df_clae2_promedio_joint$w_mean[na_indices - 1],
+  Next = df_clae2_promedio_joint$w_mean[na_indices + 1]
+)
+
+library(zoo)
+
+# aplicamos la interpolacíon
+df_clae2_promedio_joint$w_mean <- na.approx(df_clae2_promedio_joint$w_mean)
+
+# revisamos
+sum(is.na(df_clae2_promedio_joint$w_mean))
+df_clae2_promedio_joint[c(87, 88, 89), ]
+
+
+
 # Sectores elegidos: enseñanza, salud, Construcción, servicio de transporte, servicio de financiera y seguros. 
 
 
@@ -88,8 +127,6 @@ sectores_filter <- df_clae2_promedio_joint %>%
   arrange(letra, nom_provincia)
 
 sectores_filter
-
-
 
 
 
